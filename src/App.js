5b9d8bb017ee832309943b23a9750632004c7778
mani g <mani@maricitech.com>
2023-01-28 20:40:47 +0530

    "inital web setup"

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import loading from './loading.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={loading} className="App-logo" alt="logo" />
+        <p>
+          Marici Technologies PVT.LTD under bulding.
+        </p>
+        <a
+          className="App-link"
+          href="mailto:mani.maricitech.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact Marici support team.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
